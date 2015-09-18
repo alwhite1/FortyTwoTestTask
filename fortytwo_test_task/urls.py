c8bfc4304a626bb0c52b023264990b110dbae01e
alwhite1 <alwhite1@rambler.ru>
2015-09-19 01:06:48 +0300

    ticket:1 add linl to static in url

--- fortytwo_test_task/urls.py
+++ fortytwo_test_task/urls.py
@@ -1,9 +1,14 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+
+
 admin.autodiscover()
 
 urlpatterns = patterns(
     '',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', 'apps.person.views.main', name='main')
-)
+) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL,
+                                                                             document_root=settings.MEDIA_ROOT)
