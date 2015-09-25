567021e2ed601ea0d20c25a0e196bc180d16ddb8
alwhite1 <alwhite1@rambler.ru>
2015-09-25 15:41:35 +0300

    ticket:5 fix delete old photo and resizing

--- apps/person/models.py
+++ apps/person/models.py
@@ -7,6 +7,15 @@ import os
 from django.conf import settings
 
 
+def cut_image(photo):
+    img = Image.open(StringIO.StringIO(photo.read()))
+    output = StringIO.StringIO()
+    img = ImageOps.fit(img, (200, 200))
+    img.save(output, format='JPEG', quality=99)
+    output.seek(0)
+    return InMemoryUploadedFile(output, 'ImageField', photo.name, 'image/jpeg', output.len, None)
+
+
 def make_upload_path(filename):
         return u"".join(os.path.split(settings.BASE_DIR)[:len(os.path.split(settings.BASE_DIR)) - 1]) +\
                u'/uploads/images/%s' % filename
@@ -29,17 +38,17 @@ class Person(models.Model):
 
     def save(self, *args, **kwargs):
         if Person.objects.count() > 0:
+
+            if self.photo.name != Person.objects.last().photo.name:
+                old_photo_name = Person.objects.last().photo.name.split('/')[-1]
+                if old_photo_name:
+                    os.remove(make_upload_path(old_photo_name))
+                self.photo = cut_image(self.photo)
+
+        elif Person.objects.count() == 0:
             if self.photo:
-                person = Person.objects.last()
-                old_photo = make_upload_path(person.photo.name.split('/')[len(person.photo.name.split("/")) - 1])
-                os.remove(old_photo)
-        if self.photo:
-            img = Image.open(StringIO.StringIO(self.photo.read()))
-            output = StringIO.StringIO()
-            img = ImageOps.fit(img, (200, 200))
-            img.save(output, format='JPEG', quality=99)
-            output.seek(0)
-            self.photo = InMemoryUploadedFile(output, 'ImageField', self.photo.name, 'image/jpeg', output.len, None)
+                self.photo = cut_image(self.photo)
+
         super(Person, self).save(*args, **kwargs)
 
     def __unicode__(self):
