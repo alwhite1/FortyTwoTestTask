0787246753da02e9a00729815f1e449b68e11cc8
alwhite1 <alwhite1@rambler.ru>
2015-10-14 14:44:26 +0300

    ticket:1 refactor test to use factory_boy

--- apps/person/factories.py
+++ apps/person/factories.py
@@ -1,9 +1,13 @@
+# -*- coding: utf-8 -*-
 import factory
 from apps.person.models import Person
 
+
 class SimplePersonFactory(factory.Factory):
 
-    FACTORY_FOR = Person
+    class Meta:
+        model = Person
+
     name = u"new"
     last_name = u"new"
     date_of_birth = "2001-02-02"
@@ -13,9 +17,12 @@ class SimplePersonFactory(factory.Factory):
     skype = u"new"
     other_contacts = u"new"
 
+
 class CyrillicPersonFactory(factory.Factory):
 
-    FACTORY_FOR = Person
+    class Meta:
+        model = Person
+
     name = u"Имя"
     last_name = u"Фамилия",
     date_of_birth = "2001-02-02"
