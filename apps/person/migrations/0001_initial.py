8e7937d8a9e1fa9e8958b751065f4031005df875
alwhite1 <alwhite1@rambler.ru>
2015-10-23 10:50:38 +0300

    ticket:3 change migration

--- apps/person/migrations/0001_initial.py
+++ apps/person/migrations/0001_initial.py
@@ -9,7 +9,6 @@ class Migration(SchemaMigration):
 
     def forwards(self, orm):
         # Adding model 'Person'
-        db.delete_table(u'person_person')
         db.create_table(u'person_person', (
             (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('name', self.gf('django.db.models.fields.CharField')(max_length=16)),
